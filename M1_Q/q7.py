baefb80d93340cbd9ac0d7e1beddca4694cbbfdb
Rashmi nayak <patlarashmi417@gmail.com>
2019-07-23 16:33:55 +0530

    Assignments

--- M1_Q/q7.py
+++ M1_Q/q7.py
@@ -1,12 +1,27 @@
-'''7.	Write a program to accept a number from the user and determine the sum of digits of that number. Repeat the operation until the sum gets to be a single digit number.'''
+'''7.Write a program to accept a number from the user and determine the sum of digits of that number. Repeat the operation until the sum gets to be a single digit number.'''
+
+sum = 0
+
+count = 0
 num = int(input("enter the number:"))
 while num > 0:
-    sum = 0
     rem = num % 10
+    count += 1
     sum = sum + rem
     num = num // 10
-    if  sum >= 0 and sum <=9:
-        print(sum)
+while count != 0:
+    tsum = 0
+    rem = sum % 10
+    tsum +=rem
+    sum = sum //10
+    while tsum >=0 and tsum <=9:
+        rem = tsum % 10
+        
+        
+    print(tsum)
+
+
+    
 
 
 
